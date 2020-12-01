64fbe1c7a21b51787ef459f28ab7baa069823afb
Stephen Green <SGreen@qicglobal.com>
2020-12-01 16:35:43 +0000

    Add project files.

---
+++ Something.UI/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Something.UI
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddHttpClient<ISomethingService, SomethingService>();
+            var provider = services.BuildServiceProvider();
+            var somethingService = provider.GetService<ISomethingService>();
+            try
+            {
+                Console.WriteLine(somethingService.Run(args));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+            }
+        }
+    }
+}
